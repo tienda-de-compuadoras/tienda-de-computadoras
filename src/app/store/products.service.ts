370325fe9046a226101bf4fb9d543140c8f28697
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-05-08 13:02:57 -0400

    changes

--- src/app/store/products.service.ts
+++ src/app/store/products.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { Observable } from 'rxjs';
 
 import { AngularFirestore } from '@angular/fire/firestore';
 
